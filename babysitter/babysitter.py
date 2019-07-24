441e8f0d2901653cc4d8e7308c24c363e06a31fa
Jake <jayhay28324@gmail.com>
2019-07-24 18:09:18 -0400

    Refactored start time and end time to be part of the sitter object

--- babysitter/babysitter.py
+++ babysitter/babysitter.py
@@ -2,14 +2,16 @@
 
 
 class Sitter:
-    def __init__(self):
+    def __init__(self, start_time=0, end_time=0):
         self.rate = 10
+        self.start_time = start_time
+        self.end_time = end_time 
 
-    def babysit(self, hour_start=17, hour_end=17):
+    def babysit(self):
         """ Main function defining a babysitting 'event' """
-        if hour_start < 0 or (4 < hour_start < 17):
+        if self.start_time < 0 or (4 < self.start_time < 17):
             return 'ERROR: Start time out of range'
-        if hour_end < hour_start:
+        if self.end_time < self.start_time:
             return 'ERROR: Cannot end before start'
-        total = self.rate * (hour_end - hour_start)
+        total = self.rate * (self.end_time - self.start_time)
         return f'Total amount owed: ${total:.2f}'
--- test/babysitter_test.py
+++ test/babysitter_test.py
@@ -13,27 +13,27 @@ def test_event_occurred():
 def test_event_occurred_with_hours():
     """ Putting the time babysitting by giving the start and end time. """
     expected = "Total amount owed: $10.00"
-    e1 = Sitter()
+    e1 = Sitter(17, 18)
     # Before custom rates are added, using standard $10/hr.
-    assert expected == e1.babysit(17, 18)
+    assert expected == e1.babysit()
 
 
 def test_event_started_before_time_allowed():
     """ Attempting to start and event before 5 PM should return an error. """
     expected = "ERROR: Start time out of range"
-    e1 = Sitter()
-    assert expected == e1.babysit(16, 17)
+    e1 = Sitter(16, 17)
+    assert expected == e1.babysit()
 
 
 def test_event_started_after_midnight():
     """ Babysitting events can start after midnight as long as they end before 4 AM. """
     expected = "Total amount owed: $10.00"
-    e1 = Sitter()
-    assert expected == e1.babysit(0, 1)
+    e1 = Sitter(0, 1)
+    assert expected == e1.babysit()
 
 
 def test_end_time_earlier_than_start_time():
     """ Ensuring that you cannot complete an event before it is started. """
     expected = "ERROR: Cannot end before start"
-    e1 = Sitter()
-    assert expected == e1.babysit(19, 17)
+    e1 = Sitter(19, 17)
+    assert expected == e1.babysit()
