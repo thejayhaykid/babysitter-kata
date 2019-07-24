fc2257d394a92214d79f0aa0dfa9dc0e181a5d41
Jake <jayhay28324@gmail.com>
2019-07-23 21:39:19 -0400

    Babysitting event can start after midnight if it occurs before 4 AM

--- babysitter/babysitter.py
+++ babysitter/babysitter.py
@@ -7,7 +7,7 @@ class Sitter:
 
     def babysit(self, hour_start=17, hour_end=17):
         """ Main function defining a babysitting 'event' """
-        if hour_start < 17:
+        if hour_start < 0 or (4 < hour_start < 17):
             return 'ERROR: Start time out of range'
         total = self.rate * (hour_end - hour_start)
         return f'Total amount owed: ${total:.2f}'
