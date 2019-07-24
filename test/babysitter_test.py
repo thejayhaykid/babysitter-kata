18ece61d08d1769980b9a7c8843ae75df2aa3783
Jake <jayhay28324@gmail.com>
2019-07-23 20:47:53 -0400

    Added test for range of babysitting event
    Failing

--- test/babysitter_test.py
+++ test/babysitter_test.py
@@ -8,3 +8,11 @@ def test_event_occurred():
     expected = "Total amount owed: $0.00"
     e1 = Sitter()
     assert expected == e1.babysit()
+
+
+def test_event_occurred_with_hours():
+    """ Putting the time babysitting by giving the start and end time. """
+    expected = "Total amount owed: $10.00"
+    e1 = Sitter()
+    # Before custom rates are added, using standard $10/hr.
+    assert expected == e1.babysit(1700, 1800)
