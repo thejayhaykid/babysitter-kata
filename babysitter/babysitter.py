a9c94a21e8b450244f01425b6cb3a0408e1ffd1b
Jake <jayhay28324@gmail.com>
2019-07-23 21:29:28 -0400

    Start hour now checked to be after 5 PM (1700)

--- babysitter/babysitter.py
+++ babysitter/babysitter.py
@@ -5,7 +5,9 @@ class Sitter:
     def __init__(self):
         self.rate = 10
 
-    def babysit(self, hour_start=0, hour_end=0):
+    def babysit(self, hour_start=17, hour_end=17):
         """ Main function defining a babysitting 'event' """
+        if hour_start < 17:
+            return 'ERROR: Start time out of range'
         total = self.rate * (hour_end - hour_start)
         return f'Total amount owed: ${total:.2f}'
