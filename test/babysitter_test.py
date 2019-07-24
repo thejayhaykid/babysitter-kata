3c73d70ea74c15f297537aecfb8dae1e5b68bbc3
Jake <jayhay28324@gmail.com>
2019-07-23 20:07:16 -0400

    First failing test.

--- test/babysitter_test.py
+++ test/babysitter_test.py
@@ -1,1 +1,10 @@
 """ The test module for the babysitter class """
+
+from babysitter import Sitter
+
+
+def test_event_occurred():
+    """ First test similuating a babysitting event occurred. """
+    expected = "Successful event."
+    e1 = Sitter()
+    assert expected == e1.babysit()
