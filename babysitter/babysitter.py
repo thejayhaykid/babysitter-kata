c6cc408800e81a4d7d957e2489f851229e135a55
Jake <jayhay28324@gmail.com>
2019-07-24 20:34:44 -0400

    Valid time input now checked using built in Python time module.

--- babysitter/babysitter.py
+++ babysitter/babysitter.py
@@ -1,5 +1,6 @@
 """ This is the babysitter class """
 
+import time
 
 class Sitter:
     def __init__(self, start_time="0000", end_time="0000"):
@@ -9,6 +10,13 @@ class Sitter:
 
     def babysit(self):
         """ Main function defining a babysitting 'event' """
+
+        # Check for valid times
+        _, start_pass = self.validate_time(self.start_time)
+        _, end_pass = self.validate_time(self.end_time)
+
+        if not start_pass or not end_pass:
+            return 'ERROR: Please enter a valid time'
         
         # Converting strings to ints to satisfy current testing
         self.start_time = int(self.start_time) / 100
@@ -22,3 +30,12 @@ class Sitter:
             return 'ERROR: Cannot end before start'
         total = self.rate * (self.end_time - self.start_time)
         return f'Total amount owed: ${total:.2f}'
+
+
+    def validate_time(self, input_time):
+        """ Method to validate time. """
+        try:
+            ret_time = time.strptime(input_time, '%H%M')
+            return ret_time, True
+        except ValueError:
+            return None, False
