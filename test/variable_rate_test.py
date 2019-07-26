1c94ccd0b1d27d49b4d0697aa30e90e8a4cf3ad3
Jake <jayhay28324@gmail.com>
2019-07-25 20:49:19 -0400

    First test with a variable rate

--- test/variable_rate_test.py
+++ test/variable_rate_test.py
@@ -7,3 +7,9 @@ def test_with_family_and_zero_hours_worked():
     expected = "Total amount owed: $0.00"
     e1 = Sitter(family="A")
     assert expected == e1.babysit()
+
+def test_with_family_a_rate():
+    """ Testing with a variable rate with Family A to make sure rate is applied correctly. """
+    expected = "Total amount owed: $15.00"
+    e1 = Sitter("1717", "1745", "A")
+    assert expected == e1.babysit()
