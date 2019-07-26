1f2abfaa1fffb245b0fb67d0e931e7e6bdda5578
Jake <jayhay28324@gmail.com>
2019-07-25 21:00:15 -0400

    Variable Rates implemented for all three families

--- babysitter/babysitter.py
+++ babysitter/babysitter.py
@@ -2,19 +2,28 @@
 
 import time
 
+# Families rates will be global variables
+FAMILY_A = [15, 15, 15, 15, 15, 15, 20, 20, 20, 20, 20]
+FAMILY_B = [12, 12, 12, 12, 12,  8,  8, 16, 16, 16, 16]
+FAMILY_C = [21, 21, 21, 21, 15, 15, 15, 15, 15, 15, 15]
+WORKED_START = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 class Sitter:
-    def __init__(self, start_time="0000", end_time="0000"):
-        self.rate = 10
+    def __init__(self, start_time="0000", end_time="0000", family="A"):
         self.start_time = start_time
         self.end_time = end_time
         self.hours_worked = 0
+        self.family = family
+        self.hours_worked_list = WORKED_START
 
     def babysit(self):
         """ Main function defining a babysitting 'event' """
+        families = {"A": FAMILY_A, "B": FAMILY_B, "C": FAMILY_C}
+        rate = families[self.family]
 
         # Check for valid times
-        _, start_pass = self._validate_time(self.start_time)
-        _, end_pass = self._validate_time(self.end_time)
+        start_pass = self._validate_time(self.start_time)
+        end_pass = self._validate_time(self.end_time)
 
         if not start_pass or not end_pass:
             return 'ERROR: Please enter a valid time'
@@ -38,17 +47,17 @@ class Sitter:
             print(f'Hours worked: {self.hours_worked}')
             return 'ERROR: Cannot end before start'
 
-        total = self.rate * self.hours_worked
+        total = sum([i*j for i,j in zip(rate,self.hours_worked_list)])
         return f'Total amount owed: ${total:.2f}'
 
 
     def _validate_time(self, input_time):
         """ Method to validate time. """
         try:
-            ret_time = time.strptime(input_time, '%H%M')
-            return ret_time, True
+            time.strptime(input_time, '%H%M')
+            return True
         except ValueError:
-            return None, False
+            return False
 
     def _calculate_time_worked(self):
         """ Private method to calculate the time worked to make babysit() smaller."""
@@ -63,6 +72,9 @@ class Sitter:
             temp_end_time = self.end_time
         
         self.hours_worked = temp_end_time - temp_start_time
+        start_index = temp_start_time - 17
+        self.hours_worked_list = [1 if i in range(start_index, start_index + self.hours_worked) else e for i, e in enumerate(self.hours_worked_list)]
+        print(f'Hours worked list: {self.hours_worked_list}')
 
     def _round_hours(self):
         """ Private method to make sure there are no partial hours.  """
